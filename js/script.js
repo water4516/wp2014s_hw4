73ec03b374147e679f17637cee5f0b3f84dbc9f7
Jerry <100703009@nccu.edu.tw>
2014-06-11 15:00:51 +0800

    5

--- js/script.js
+++ js/script.js
@@ -21,10 +21,11 @@ FB.getLoginStatus(function(response) {
     //呼叫api把圖片放到#preview IMG tag 內
 	uid = response.authResponse.userID;
 	accessToken = response.authResponse.accessToken;
-	FB.api('/me/picture?type=normal', function (response) {
-		var str="<img src="+ response.data.url +">";
-		$('preview1').append(str);
+	FB.api('/me/picture?type=normal', function(response) { // normal/large/squere 
+		var str="<img id=preview1 crossOrigin='Anonymous' src="+ response.data.url +">";
+		$('body').append(str);
 	});
+	alertify.alert(response.data.url);
   } else if (response.status === 'not_authorized') {
     //要求使用者登入，索取publish_actions權限
 	console.log("this user is not authorizied your apps");
@@ -152,7 +153,7 @@ FB.getLoginStatus(function(response) {
 
 
 
-// Post a BASE64 Encoded PNG Image to facebook，以下程式為把照片po到facebook的方法，基本上這樣就可以不用動了，但思考authToken該怎麼拿到，因為這裡我並沒有把使用者登入的token載入到這函數內，所以它是不會得到token的
+// Post a BASE64 Encoded PNG Image to facebook，以下程式為把照片po到facebook的方法，基本上這樣就可以不用動了，但思考accessToken該怎麼拿到，因為這裡我並沒有把使用者登入的token載入到這函數內，所以它是不會得到token的
 function PostImageToFacebook() {
 	$('.info').append('<img src="img/loading.gif"/>')//載入loading的img
     var canvas = document.getElementById("canvas");//找canvas
@@ -163,12 +164,12 @@ function PostImageToFacebook() {
         console.log(e);//錯誤訊息的log
     }
     var fd = new FormData();
-    fd.append("access_token", authToken);//請思考accesstoken要怎麼傳到這function內
+    fd.append("access_token", accessToken);//請思考accesstoken要怎麼傳到這function內
     fd.append("source", blob);//輸入的照片
     fd.append("message", "這是HTML5 canvas和Facebook API結合教學");//輸入的訊息
     try {
         $.ajax({
-            url: "https://graph.facebook.com/me/photos?access_token=" + authToken,//GraphAPI Call
+            url: "https://graph.facebook.com/me/photos?access_token=" + accessToken,//GraphAPI Call
             type: "POST",
             data: fd,
             processData: false,
