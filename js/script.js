24e8fafda2636bfd58e546c56f5ceee0ae5d9422
Jerry <100703009@nccu.edu.tw>
2014-06-11 18:43:04 +0800

    7

--- js/script.js
+++ js/script.js
@@ -103,8 +103,8 @@ FB.getLoginStatus(function(response) {
           	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 			var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
-			canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
-			canvas.height = profileIMG.height;
+			//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
+			//canvas.height = profileIMG.height;
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
 			ctx.drawImage(img3,200,400); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			ctx.drawImage(img2,0,0); //劃入img2
